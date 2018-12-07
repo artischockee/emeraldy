61b3dc5a41cd27d658dd7965820bfe758376a934
artyeug <artyeug@gmail.com>
2018-12-07 20:14:12 +0700

    Add form reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
-// import { reducer as formReducer } from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
 import value from './value';
 
 export default combineReducers({
-  value
+  value,
+  form: formReducer
 });
