d602fe1fe2f92c2f29c26af3b43f0a82e8bd7688
artyeug <artyeug@gmail.com>
2018-12-07 20:14:07 +0700

    Change components

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,31 @@
 import React from 'react';
-// import { hot } from 'react-hot-loader';
 import './sass/App.sass';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
-class App extends React.Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-    this.setState({ responseToPost: body });
-  };
+import { Switch, Route } from 'react-router-dom';
+import Login from './components/login/Login';
 
+class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Header />
-        <Main />
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
-        <Footer />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Body} />
+        </Switch>
       </React.Fragment>
     );
   }
 }
 
-// export default hot(module)(App);
+const Body = () => (
+  <React.Fragment>
+    <Header />
+    <Main />
+    <Footer />
+  </React.Fragment>
+)
+
 export default App;
