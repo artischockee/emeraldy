18f9a706fb3245549f68fa2e74fc9f0c9f785ae9
artyeug <artyeug@gmail.com>
2018-12-07 20:10:37 +0700

    Add login button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -24,7 +24,7 @@ const Header = () => (
         </ul>
       </nav>
       <div className="header__login">
-        <p style={{margin: 0}}>Log in</p>
+        <Link className="login-button" to="/login">Log in</Link>
       </div>
     </div>
   </header>
