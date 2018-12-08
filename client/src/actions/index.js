9b5f0126a56c157cef24f389ded4de0cc12832d6
artyeug <artyeug@gmail.com>
2018-12-09 01:50:51 +0700

    Remove unused variables

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,17 +1,1 @@
-export const Value = {
-  INC: 'INC',
-  DEC: 'DEC',
-  NUL: 'NUL'
-};
-
-export const increment = () => ({
-  type: Value.INC
-})
-
-export const decrement = () => ({
-  type: Value.DEC
-})
-
-export const nullify = () => ({
-  type: Value.NUL
-})
+export * from './scholastic';
