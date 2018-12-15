65bb15910916931fbfc006018320c8e459b88713
artyeug <artyeug@gmail.com>
2018-12-15 17:44:47 +0700

    Initial commit for projects-related actions

---
+++ client/src/actions/projects.js
@@ -0,0 +1,24 @@
+const ENTITY = "Projects";
+
+export const Projects = {
+  LOAD_DATA: `${ENTITY} LOAD DATA`,
+  UNLOAD_DATA: `${ENTITY} UNLOAD DATA`
+};
+
+export const loadData = (data) => ({
+  type: Projects.LOAD_DATA,
+  payload: data
+});
+
+export const unloadData = () => ({
+  type: Projects.UNLOAD_DATA
+});
+
+export const fetchData = (data) => (dispatch) => (
+  fetch('/api/projects')
+    .then(
+      response => response.json(),
+      error => console.error('An error occured', error)
+    )
+    .then(projects => dispatch(loadData(projects)))
+);
