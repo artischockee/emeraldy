8f6d244a0dc042f08b63716cc0c3f27cb5cc9407
artyeug <artyeug@gmail.com>
2018-12-16 23:41:01 +0700

    Use morgan library for logging

--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const scholasticRouter = require('./scholasticRouter');
 const projectsRouter = require('./projectsRouter');
 
 const apiRouter = express.Router();
 
+apiRouter.use(morgan('dev'));
 apiRouter.use('/scholastic', scholasticRouter);
 apiRouter.use('/projects', projectsRouter);
 
