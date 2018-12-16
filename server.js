75e97543254dc56ec27e7acf3f469a6f69422e70
artyeug <artyeug@gmail.com>
2018-12-16 23:39:04 +0700

    Use switch for NODE_ENV mode

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const errorhandler = require('errorhandler');
 const apiRouter = require('./server/routers/apiRouter');
 
 const app = express();
@@ -9,15 +10,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
 
-/*
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+switch (process.env.NODE_ENV) {
+  case 'development':
+    app.use(errorhandler())
+    break;
+  case 'production':
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+    break;
+  default:
+    throw new Error(`Unknown NODE_ENV value (${process.env.NODE_ENV})`);
 }
-*/
+
+// app.use((err, req, res, next) => {
+//   if (!err.status)
+//     err.status = 500;
+//
+//   return res.status(err.status).send(err.message);
+// });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
