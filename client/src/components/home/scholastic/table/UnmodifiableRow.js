ca51074c96486db4e8360efa9cce7f1ce8d2ba7e
artyeug <artyeug@gmail.com>
2018-12-25 00:58:39 +0700

    Replace ordinary buttons with SVG ones

--- client/src/components/home/scholastic/table/UnmodifiableRow.js
+++ client/src/components/home/scholastic/table/UnmodifiableRow.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { loadData } from '../../../../actions';
+import { loadData, invokeDeleteDialog } from '../../../../actions';
+import SVGTrashBin from '../../../svg/TrashBin';
+import SVGEdit from '../../../svg/Edit';
 
 class UnmodifiableRow extends React.Component {
   handleEdit = () => {
-    const { date, hours, minutes, onEdit, load } = this.props;
+    const { date, hours, minutes, projectId, onEdit, load } = this.props;
 
-    load({ date, hours, minutes });
+    load({ date, hours, minutes, projectId });
     onEdit();
   };
 
   render() {
-    const { id, date, hours, minutes, onDelete } = this.props;
+    const { id, projectName, date, hours, minutes, onDelete, invokeDeleteDialog } = this.props;
 
     return (
       <tr>
-        <td>{id}</td>
+        <td>{projectName}</td>
         <td>{new Date(date).toDateString()}</td>
         <td>{hours}</td>
         <td>{minutes}</td>
-        <td>
-          <button onClick={() => onDelete(id)}>Delete</button>
-          <button onClick={this.handleEdit}>Edit</button>
+        <td className="scholastic-table__cell scholastic-table__cell--actions">
+          <button
+            className="scholastic-table__button"
+            onClick={this.handleEdit}
+          >
+            <SVGEdit className="button__svg button__svg--edit" />
+            Edit
+          </button>
+          <button
+            className="scholastic-table__button scholastic-table__button--delete"
+            onClick={() => invokeDeleteDialog(id, () => onDelete(id))}
+            // onClick={() => onDelete(id)}
+            >
+              <SVGTrashBin className="button__svg button__svg--delete" />
+            </button>
         </td>
       </tr>
     )
@@ -29,7 +43,8 @@ class UnmodifiableRow extends React.Component {
 }
 
 const mapDispatchToProps = {
-  load: loadData
+  load: loadData,
+  invokeDeleteDialog
 };
 
 export default connect(null, mapDispatchToProps)(UnmodifiableRow);
