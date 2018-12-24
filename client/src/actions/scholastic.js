d27fff18f169e70b17415466c142e7de66901bf3
artyeug <artyeug@gmail.com>
2018-12-25 00:56:32 +0700

    Add new actions

--- client/src/actions/scholastic.js
+++ client/src/actions/scholastic.js
@@ -2,7 +2,9 @@ const ENTITY = "Scholastic";
 
 export const Scholastic = {
   LOAD_DATA: `${ENTITY} LOAD DATA`,
-  UNLOAD_DATA: `${ENTITY} UNLOAD DATA`
+  UNLOAD_DATA: `${ENTITY} UNLOAD DATA`,
+  INVOKE_DELETE_DIALOG: `${ENTITY} INVOKE DELETE DIALOG`,
+  CLOSE_DIALOG: `${ENTITY} CLOSE DIALOG`
 };
 
 export const loadData = (data) => ({
@@ -12,4 +14,14 @@ export const loadData = (data) => ({
 
 export const unloadData = () => ({
   type: Scholastic.UNLOAD_DATA
+});
+
+export const invokeDeleteDialog = (objectIdToDelete, callback) => ({
+  type: Scholastic.INVOKE_DELETE_DIALOG,
+  payload: objectIdToDelete,
+  callback
+});
+
+export const closeDialog = () => ({
+  type: Scholastic.CLOSE_DIALOG,
 });
