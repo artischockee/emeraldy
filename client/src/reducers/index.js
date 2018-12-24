96c89eeaf945861ee7245ebc8ecd3c860e1d0a4c
artyeug <artyeug@gmail.com>
2018-12-25 00:57:39 +0700

    Add new reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import scholasticEditEntry from './scholasticEditEntry';
+import projects from './projects';
+import modalDialog from './modalDialog';
 
 export default combineReducers({
   scholasticEditEntry,
+  projects,
+  modalDialog,
   form: formReducer
 });
