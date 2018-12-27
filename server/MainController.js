bcf9f5e8ef9022219487c5d332ac39f214829300
artyeug <artyeug@gmail.com>
2018-12-27 16:09:12 +0700

    Remove all redundant code; prepare the component for deletion

--- server/MainController.js
+++ server/MainController.js
@@ -2,36 +2,11 @@ const scholastic = require('./scholastic');
 const projects = require('./projects');
 const database = require('./database');
 
-console.log('fuck off from MainController');
-
 class MainController {
   constructor(db) {
     this.db = db;
     this._scholastic = scholastic;
     this._projects = projects;
-  }
-
-  get scholastic() {
-    return this._scholastic;
-  }
-
-  set scholastic(sch) {}
-
-  get projects() {
-    return this._projects;
-  }
-
-  set projects(prj) {}
-
-  selectScholasticTable() {
-    const { _scholastic: s, _projects: p, db } = this;
-
-    const sch = s.tableName;
-    const prj = p.tableName;
-
-    const sql = `SELECT ${sch}.rowid, ${prj}.name AS projectName, ${Object.keys(s.columns)} FROM ${sch} JOIN ${prj} ON ${prj}.rowid = ${sch}.projectId`;
-
-    return db.all(sql);
   }
 
   getProjectsCount() {
