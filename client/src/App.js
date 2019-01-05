793fe21d2cf5d72ae652e5b9ba162b0996e7a2b4
artyeug <artyeug@gmail.com>
2019-01-05 14:12:36 +0700

    Replace Body with a bunch of components; remove Main component

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import './sass/App.sass';
 import Header from './components/Header';
-import Main from './components/Main';
 import Footer from './components/Footer';
+import Home from './components/home/Home';
+import Scholastic from './components/home/scholastic/Scholastic';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Crud from './components/crud/Crud';
 import ModalDialogSystem from './components/ModalDialogSystem';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from './components/login/Login';
+import { Switch, Route } from 'react-router-dom';
+
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/" component={Body} />
-        </Switch>
-      </Router>
+      <React.Fragment>
+        <ModalDialogSystem />
+        <Header />
+        <main className="main">
+          <div className="section-wrapper">
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/scholastic" component={Scholastic} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/crud-test" component={Crud} />
+              <Route component={Error404} />
+            </Switch>
+          </div>
+        </main>
+        <Footer />
+      </React.Fragment>
     );
   }
 }
 
-const Body = () => (
-  <React.Fragment>
-    <ModalDialogSystem />
-    <Header />
-    <Main />
-    <Footer />
-  </React.Fragment>
-)
+const Error404 = () => <h1>Page not found!</h1>;
 
 export default App;
