c175a389772a3d77130b02f115b8aacd46d7fc02
artyeug <artyeug@gmail.com>
2019-01-18 16:06:53 +0700

    Add mongoose package

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const errorhandler = require('errorhandler');
 const apiRouter = require('./routes/api');
@@ -6,6 +7,12 @@ const apiRouter = require('./routes/api');
 function initialize() {
   const app = express();
   const PORT = process.env.PORT || 5000;
+
+  mongoose.connect('mongodb://localhost/new_database', { useNewUrlParser: true });
+
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  db.once('open', () => console.log('Connected to MongoDB'));
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
