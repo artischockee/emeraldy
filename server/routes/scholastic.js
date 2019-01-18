a4a82cd226f99a0159543f4124c5d0407b0fa322
artyeug <artyeug@gmail.com>
2019-01-18 16:08:41 +0700

    Remove redundant code

--- server/routes/scholastic.js
+++ server/routes/scholastic.js
@@ -12,20 +12,7 @@ scholasticRouter.post('/', controller.createEntry);
 scholasticRouter.put('/:entryId', controller.editEntry);
 scholasticRouter.delete('/:entryId', controller.deleteEntry);
 
-/*
-scholasticRouter.get('/tableCreate', (req, res) => {
-  mainController.scholastic.createTable().then(
-    (data) => res.send(JSON.stringify(data)),
-    (error) => res.status(400).send(JSON.stringify(error))
-  );
-});
-
-scholasticRouter.get('/tableDrop', (req, res) => {
-  mainController.scholastic.dropTable().then(
-    (data) => res.send(JSON.stringify(data)),
-    (error) => res.status(400).send(JSON.stringify(error))
-  );
-});
-*/
+// scholasticRouter.get('/tableCreate');
+// scholasticRouter.get('/tableDrop');
 
 module.exports = scholasticRouter;
