f3147bb094cb97ad19718c64423181fb3b1b1dce
artyeug <artyeug@gmail.com>
2019-01-18 16:11:00 +0700

    Change required filename

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const controller = require('../controllers/projects');
+const controller = require('../controllers/project');
 
 const projectsRouter = express.Router();
 
