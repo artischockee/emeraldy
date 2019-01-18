31d4a1c5962bb0ffc894f91938f8a6ab93e93156
artyeug <artyeug@gmail.com>
2019-01-18 16:14:22 +0700

    Ease up App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,15 @@
-import React from 'react';
 import './sass/App.sass';
-import Header from './components/Header';
+import React from 'react';
+import Main from './components/Main';
 import Footer from './components/Footer';
-import Home from './components/home/Home';
-import Scholastic from './components/home/scholastic/Scholastic';
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import Crud from './components/crud/Crud';
 import ModalDialogSystem from './components/ModalDialogSystem';
 
-import { Switch, Route } from 'react-router-dom';
-
-
-class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <ModalDialogSystem />
-        <Header />
-        <main className="main">
-          <div className="section-wrapper">
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/scholastic" component={Scholastic} />
-              <Route path="/about" component={About} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/crud-test" component={Crud} />
-              <Route component={Error404} />
-            </Switch>
-          </div>
-        </main>
-        <Footer />
-      </React.Fragment>
-    );
-  }
-}
-
-const Error404 = () => <h1>Page not found!</h1>;
+const App = () => (
+  <React.Fragment>
+    <ModalDialogSystem />
+    <Main />
+    <Footer />
+  </React.Fragment>
+);
 
 export default App;
