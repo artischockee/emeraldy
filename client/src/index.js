d75e592e43541119c285e754afd883cec5a792b6
artyeug <artyeug@gmail.com>
2019-01-18 16:16:13 +0700

    Wrap root components in a class

--- client/src/index.js
+++ client/src/index.js
@@ -9,31 +9,44 @@ import rootReducer from './reducers';
 import App from './App';
 import Login from './components/login/Login';
 import * as serviceWorker from './serviceWorker';
+import { timeout } from './auxiliary/timeout';
 
 const store = createStore(
   rootReducer,
   applyMiddleware(thunk)
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login" component={Login} />
-        {/* <Route path="/resetPassword/:token" component={Login} /> */}
-        <Route path="/" component={App} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+
+class Root extends React.Component {
+  async componentDidMount() {
+    await timeout(300);
+    rootElement.classList.add("visible");
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login" component={Login} />
+            {/* <Route path="/resetPassword/:token" component={Login} /> */}
+            <Route path="/" component={App} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
+
+ReactDOM.render(<Root />, rootElement);
 
 document.addEventListener('keyup', (e) => {
-    if (e.key === 'Control') {
-        console.group("Store state")
-        console.log(store.getState())
-        console.groupEnd()
-    }
+  if (e.key === 'Control') {
+    console.group("Store state")
+    console.log(store.getState())
+    console.groupEnd()
+  }
 })
 
 // If you want your app to work offline and load faster, you can change
