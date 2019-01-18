6d2167f9ac5a1c93c5faaed704a6a871e1039b7d
artyeug <artyeug@gmail.com>
2019-01-18 16:14:54 +0700

    Comment all the code inside methods in Login controller

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,52 +1,52 @@
-const Login = require('../models/login');
+const Account = require('../models/account');
 
 exports.get = (req, res, next) => {
-  try {
-    Projects.selectAllRows().then(
-      (data) => res.send(JSON.stringify(data))
-    );
-  } catch (error) {
-    res.status(400).send(JSON.stringify(error));
-  }
+  // try {
+  //   Projects.selectAllRows().then(
+  //     (data) => res.send(JSON.stringify(data))
+  //   );
+  // } catch (error) {
+  //   res.status(400).send(JSON.stringify(error));
+  // }
 };
 
 exports.authenticate = (req, res, next) => {
-  try {
-    Login.checkLoginForExistence(req.body.login).then(
-      (data) => {
-        if (!data)
-          return res.status(404).send('No user found.');
-
-        req.data = data;
-        next();
-      }
-    );
-  } catch (error) {
-    res.status(400).send(JSON.stringify(error));
-  }
+  // try {
+  //   Login.checkLoginForExistence(req.body.login).then(
+  //     (data) => {
+  //       if (!data)
+  //         return res.status(404).send('No user found.');
+  //
+  //       req.data = data;
+  //       next();
+  //     }
+  //   );
+  // } catch (error) {
+  //   res.status(400).send(JSON.stringify(error));
+  // }
 };
 
 exports.logging = (req, res) => {
-  try {
-    if (req.body.password === req.data.password) {
-      console.log('login is correct...');
-      res.send('Everything is OK');
-    }
-  } catch (error) {
-    res.status(400).send(JSON.stringify(error));
-  }
+  // try {
+  //   if (req.body.password === req.data.password) {
+  //     console.log('login is correct...');
+  //     res.send('Everything is OK');
+  //   }
+  // } catch (error) {
+  //   res.status(400).send(JSON.stringify(error));
+  // }
 };
 
 exports.create = (req, res, next) => {
-  const project = new Project({
-    name: req.body.name
-  });
-
-  project.save((err) => {
-    if (err)
-      return next(err);
-    res.status(201).send('Created successfully');
-  });
+  // const project = new Project({
+  //   name: req.body.name
+  // });
+  //
+  // project.save((err) => {
+  //   if (err)
+  //     return next(err);
+  //   res.status(201).send('Created successfully');
+  // });
 
   // const { body: data } = req;
   //
