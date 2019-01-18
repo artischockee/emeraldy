27bc485e3b689b7b59012fca6905f999244975d6
artyeug <artyeug@gmail.com>
2019-01-18 16:11:29 +0700

    Replace old code with new 2 sections of Home component

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import Link from '../generic/Link';
+import Prime from '../prime/Prime';
+import Description from '../description/Description';
 
 const Home = () => (
   <React.Fragment>
-    <div className="home__link-container">
-      <Link className="link" to="/scholastic">
-          <div className="link__image" style={{ backgroundImage: "url(images/scholastic.png)" }} />
-      </Link>
-    </div>
+    <Prime />
+    <Description />
   </React.Fragment>
 );
 
