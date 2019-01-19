5142206fc9b7946dd57d547ae9321c1756819f40
artyeug <artyeug@gmail.com>
2019-01-19 22:51:29 +0700

    Finalize SuccessComponent

--- client/src/components/login/SuccessComponent.js
+++ client/src/components/login/SuccessComponent.js
@@ -1,40 +1,12 @@
 import React from 'react';
-import classNames from 'classnames';
-import { timeout } from '../../auxiliary/timeout';
+import ComponentWrapper from './ComponentWrapper';
+import SVGHappyFace from '../svg/HappyFace';
 
-class SuccessComponent extends React.Component {
-  // static headerStyle = {
-  //   backgroundImage: 'url(images/login/header-background.jpg)'
-  // };
-
-  state = {
-    shouldTranslate: false
-  };
-
-  async componentDidMount() {
-    await timeout();
-
-    this.setState({
-      shouldTranslate: true
-    });
-  }
-
-  render() {
-    const { shouldTranslate } = this.state;
-
-    return (
-      <div className={classNames(
-        "login__component login__component_last",
-        { "login__component_last-is-translating": shouldTranslate }
-      )}>
-        <div>fuck you asshole</div>
-        <div>fuck you asshole</div>
-        <div>fuck you asshole</div>
-        <div>fuck you asshole</div>
-        <div>fuck you asshole</div>
-      </div>
-    );
-  }
-}
+const SuccessComponent = () => (
+  <ComponentWrapper alignCenter>
+    <SVGHappyFace className="login__svg svg" />
+    <p className="login__welcome-message">Welcome, {'%username%'}!</p>
+  </ComponentWrapper>
+);
 
 export default SuccessComponent;
