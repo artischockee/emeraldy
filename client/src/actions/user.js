892c19ae8716623c9adb7743937d4ceea0c498e6
artyeug <artyeug@gmail.com>
2019-01-28 13:58:25 +0700

    Implement login behavior

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,17 +1,15 @@
-const ENTITY = 'USER';
+import { stopSubmit } from 'redux-form';
+import { loginFormName } from '../auxiliary/redux-forms';
+
+const ENTITY = 'User/';
 
 export const User = {
-  LOGIN_SUCCESSFUL: `${ENTITY} LOGIN_SUCCESSFUL`,
-  LOGIN_FAILED: `${ENTITY} LOGIN_FAILED`
+  LOGIN_SUCCESSFUL: `${ENTITY}LOGIN_SUCCESSFUL`
 };
 
-const assignLoginData = (data) => ({
+const acceptLogin = (data) => ({
   type: User.LOGIN_SUCCESSFUL,
   payload: data
-});
-
-const rejectLogin = () => ({
-  type: User.LOGIN_FAILED
 });
 
 export const submitLogin = (data) => (dispatch) => {
@@ -31,14 +29,10 @@ export const submitLogin = (data) => (dispatch) => {
       error => console.error(error)
     )
     .then(data => {
-      if (data.error) {
-        console.error(data.error);
-        return dispatch(rejectLogin());
-      }
-      else {
-        console.log('data', data);
-        return dispatch(assignLoginData(data));
-      }
+      if (data.hasError)
+        return dispatch(stopSubmit(loginFormName, data));
+      else
+        return dispatch(acceptLogin(data));
     })
-    // .then(smth => dispatch(assignLoginData(smth)))
+    // .then(smth => dispatch(acceptLogin(smth)))
 };
