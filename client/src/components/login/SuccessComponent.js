eca91fd21db2d66beb400d723999e552690be6b6
artyeug <artyeug@gmail.com>
2019-01-28 14:51:32 +0700

    Implement user login (or full name) displaying

--- client/src/components/login/SuccessComponent.js
+++ client/src/components/login/SuccessComponent.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ComponentWrapper from './ComponentWrapper';
 import SVGHappyFace from '../svg/HappyFace';
 
-const SuccessComponent = () => (
+const SuccessComponent = ({ user }) => (
   <ComponentWrapper alignCenter>
     <SVGHappyFace className="login__svg svg" />
-    <p className="login__welcome-message">Welcome, {'%username%'}!</p>
+    <p className="login__welcome-message">
+      Welcome, {
+        (user.firstName && user.lastName)
+          ? `${user.firstName} ${user.lastName}`
+          : user.login
+      }!
+    </p>
   </ComponentWrapper>
 );
 
-export default SuccessComponent;
+const mapStateToProps = (state, ownProps) => ({
+  user: {
+    login: state.user.login,
+    firstName: state.user.firstName,
+    lastName: state.user.lastName
+  }
+});
+
+export default connect(mapStateToProps)(SuccessComponent);
