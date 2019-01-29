d92a4d74af4ef1c88a30b024ccc3cd49f7adc4ae
artyeug <artyeug@gmail.com>
2019-01-29 20:52:15 +0700

    Extract store configure logic into separated file

--- client/src/index.js
+++ client/src/index.js
@@ -1,41 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import './index.css';
-import rootReducer from './reducers';
+import React from 'react';
+import { render } from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import { timeout } from './auxiliary';
+import configureStore from './configureStore';
+import Root from './components/Root';
 
-import Root from './Root';
+const store = configureStore();
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk)
+render(
+  <Root store={store} />,
+  document.getElementById('root')
 );
-
-const rootElement = document.getElementById('root');
-
-class RootContainer extends React.Component {
-  async componentDidMount() {
-    await timeout(300);
-    rootElement.classList.add("visible");
-  }
-
-  render() {
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <Root />
-        </BrowserRouter>
-      </Provider>
-    );
-  }
-}
-
-ReactDOM.render(<RootContainer />, rootElement);
 
 document.addEventListener('keyup', (e) => {
   if (e.key === 'Control') {
