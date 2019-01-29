9e1bd92713efa80e5f67adad32391754c7c5ff85
artyeug <artyeug@gmail.com>
2019-01-29 20:43:05 +0700

    Initial commit for ControlPanel

---
+++ client/src/components/control-panel/ControlPanel.js
@@ -0,0 +1,12 @@
+import './ControlPanel.sass';
+import React from 'react';
+
+export default class ControlPanel extends React.Component {
+  render() {
+    return (
+      <section className="control-panel">
+        <p>logo</p>
+      </section>
+    );
+  }
+}
