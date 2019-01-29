985317f24ef55f9f1127fc51bb5d708edcb312e1
artyeug <artyeug@gmail.com>
2019-01-29 20:44:43 +0700

    Export all the SVG components from the index file

---
+++ client/src/components/svg/index.js
@@ -0,0 +1,7 @@
+export * from './Bell';
+export * from './CaretDown';
+export * from './Edit';
+export * from './Emerald';
+export * from './HappyFace';
+export * from './Lens';
+export * from './TrashBin';
