0efa6eb1573125b1cd52bab78f26b46fb3d6efc1
artyeug <artyeug@gmail.com>
2019-01-29 21:00:55 +0700

    Refactor Header component

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,62 +1,34 @@
 import './Header.sass';
 import React from 'react';
-import classNames from 'classnames';
-import Link from '../generic/Link';
-import SVGEmerald from '../svg/Emerald';
-import SVGLens from '../svg/Lens';
+import { connect } from 'react-redux';
+import { submitLogout } from '../../actions';
+import { Bell } from '../svg';
+import UserBlock from './UserBlock';
 
-const Header = ({ inSection }) => (
-  <header className={classNames("header", { "header_of-section": inSection })}>
-    <div className="header__logo-container">
-      <SVGEmerald className="logo__svg" />
-      <span className="logo__text">emeraldy</span>
+const Header = ({ isLoggedIn, handleLogout }) => (
+  <header className="header">
+    <div className="header__search-form">
+      <p style={{margin: 0}}>search form</p>
     </div>
-    <nav className="header__nav-social">
-      <ul className="nav__ul">
-        <li className="nav__li nav__li--narrow">
-          <p>in</p>
-        </li>
-        <li className="nav__li nav__li--narrow">
-          <p>tw</p>
-        </li>
-        <li className="nav__li nav__li--narrow">
-          <p>vk</p>
-        </li>
-        <li className="nav__li nav__li--narrow">
-          <p>fb</p>
-        </li>
-      </ul>
-    </nav>
-    <nav className="header__nav-links">
-      <ul className="nav__ul">
-        <li className="nav__li">
-          <Link className="li__link" to="/" mainPage>Home</Link>
-        </li>
-        <li className="nav__li">
-          <Link className="li__link" to="/about">About</Link>
-        </li>
-        <li className="nav__li">
-          <Link className="li__link" to="/contact">Contact</Link>
-        </li>
-        <li className="nav__li">
-          <Link className="li__link" to="/crud-test">CRUD</Link>
-        </li>
-        <li className="nav__li">
-          <Link className="li__link" to="/login">Login</Link>
-        </li>
-        <li className="nav__li">
-          <Link className="li__link" to="/crud-tessst">bad link</Link>
-        </li>
-      </ul>
-    </nav>
-    <div className="header__tools">
-      <p style={{ marginRight: 28, textDecoration: 'underline' }}>EN</p>
-      <SVGLens className="svg__lens" />
+
+    {/* TODO: */}
+    <div className="header__notifications-area">
+      <Bell className="header__notifications-bell" />
     </div>
-    {/* <div className="header__login">
-      <Link className="login-button" to="/login">Log in</Link>
-    </div> */}
+
+    <UserBlock />
   </header>
-)
+);
 
-export default Header;
+const mapStateToProps = (state, ownProps) => ({
+  isLoggedIn: state.user.isLoggedIn
+});
+
+const mapDispatchToProps = {
+  handleLogout: submitLogout
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
