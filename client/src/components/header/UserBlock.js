4b7c51df8896f53f36e0dd9cd6328391d8bccad6
artyeug <artyeug@gmail.com>
2019-01-31 15:48:35 +0700

    Connect UserBlock to the store; replace old handicraft dropdown with new one

--- client/src/components/header/UserBlock.js
+++ client/src/components/header/UserBlock.js
@@ -1,77 +1,46 @@
 import React from 'react';
-import classNames from 'classnames';
-import Dropdown from '../generic/Dropdown';
+import { connect } from 'react-redux';
+import { getLogin, getName, hasFullName } from '../../reducers/user';
+import Dropdown from '../generic/dropdown/Dropdown';
 import { CaretDown } from '../svg';
 
 class UserBlock extends React.Component {
-  state = {
-    isDropdownDisplayed: false
-  };
-
-  // dropdownRef = React.createRef();
-
-  // switchDropdown = () => {
-  //   this.setState({
-  //     isDropdownDisplayed: !this.state.isDropdownDisplayed
-  //   })
-  // }
-
-  // detectClick = (e) => {
-  //   if (!this.state.isDropdownDisplayed)
-  //     return;
-  //
-  //   if (
-  //     [
-  //       this.dropdownRef.current,
-  //     ].every(ref => !ref.contains(e.target))
-  //     && this.state.isDropdownDisplayed
-  //   ) {
-  //     this.setState({ isDropdownDisplayed: false });
-  //   }
-  // }
-  //
-  // componentDidMount() {
-  //   window.addEventListener('click', this.detectClick);
-  // }
-  //
-  // componentWillUnmount() {
-  //   window.removeEventListener('click', this.detectClick);
-  // }
-
   render() {
-    const { isDropdownDisplayed } = this.state;
+    const {
+      userHasFullName,
+      userLogin,
+      userName
+    } = this.props;
 
     return (
-      <section className="user-block header__user-block">
-        <div
-          className="user-block__userdata-area"
-          onClick={this.switchDropdown}
-        >
+      <Dropdown component="section" className="user-block header__user-block">
+        <Dropdown.Trigger className="user-block__userdata-area">
           <div className="user-block__image" />
-          <span className="user-block__username">Artem Piskarev</span>
+          <span className="user-block__username">
+            {userHasFullName
+              ? `${userName.first} ${userName.last}`
+              : userLogin
+            }
+          </span>
           <CaretDown className="user-block__caret-down" />
-        </div>
+        </Dropdown.Trigger>
 
-        <Dropdown className="user-block__dropdown">
+        <Dropdown.Content className="user-block__dropdown">
           <ul>
             <li>View Profile</li>
             <li>Settings</li>
             <li>Log out</li>
           </ul>
-        </Dropdown>
-        {/* <div className={classNames(
-          "user-block__dropdown",
-          { "user-block__dropdown_is-visible": isDropdownDisplayed }
-        )}>
-          <ul>
-            <li>View Profile</li>
-            <li>Settings</li>
-            <li>Log out</li>
-          </ul>
-        </div> */}
-      </section>
+        </Dropdown.Content>
+      </Dropdown>
     );
   }
 }
 
-export default UserBlock;
+const mapStateToProps = (state) => ({
+  userHasFullName: hasFullName(state),
+  userLogin: getLogin(state),
+  userName: getName(state)
+});
+
+export default connect(mapStateToProps)(UserBlock);
