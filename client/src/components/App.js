124e931e3afe6b73e57570c52671ceee1125a67f
artyeug <artyeug@gmail.com>
2019-02-01 03:44:31 +0700

    Include new Main component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,8 +2,8 @@ import '../sass/App.sass';
 import React from 'react';
 import ControlPanel from './control-panel';
 import Header from './header/Header';
-import Main from './Main';
-import Footer from './Footer';
+import Main from './main/Main';
+// import Footer from './Footer';
 import ModalDialogSystem from './ModalDialogSystem';
 
 const App = () => (
@@ -12,13 +12,7 @@ const App = () => (
     <ControlPanel />
     <div className="main-part" style={{ display: 'flex', flexDirection: 'column', flex: 'auto' }}>
       <Header />
-      <main style={{
-        backgroundColor: 'lightgray',
-        flex: 'auto'
-      }}>
-        <p>test</p>
-      </main>
-      {/* <Main /> */}
+      <Main />
       {/* <Footer /> */}
     </div>
   </React.Fragment>
