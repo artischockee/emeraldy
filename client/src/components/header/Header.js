a55116e9004788451532c8916eee5babdbc30499
artyeug <artyeug@gmail.com>
2019-02-02 01:16:23 +0700

    Add search form; connect component to redux-form

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,15 +1,21 @@
 import './Header.sass';
 import React from 'react';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import classNames from 'classnames';
+import { headerSearchFormName } from '../../auxiliary/redux-forms';
 import { submitLogout } from '../../actions';
-import { Bell } from '../svg';
+import { Bell, Lens } from '../svg';
 import UserBlock from './UserBlock';
 
 const Header = ({ isLoggedIn, handleLogout }) => (
   <header className="header">
-    <div className="header__search-form">
-      <p style={{margin: 0}}>search form</p>
-    </div>
+    <Field
+      component={searchField}
+      name="query"
+      placeholder="Search"
+      type="text"
+     />
 
     {/* TODO: */}
     <div className="header__notifications-area">
@@ -20,15 +26,35 @@ const Header = ({ isLoggedIn, handleLogout }) => (
   </header>
 );
 
+const searchField = ({
+  input,
+  placeholder,
+  type,
+  meta: { active }
+}) => (
+  <div
+    className={classNames("header__search-form", {
+      "header__search-form_is-active": active || input.value
+    })}
+  >
+    <Lens className="header__search-lens" />
+    <input
+      {...input}
+      className="input header__input"
+      placeholder={placeholder}
+      type={type}
+    />
+  </div>
+);
+
 const mapStateToProps = (state, ownProps) => ({
   isLoggedIn: state.user.isLoggedIn
 });
 
-const mapDispatchToProps = {
+const actions = {
   handleLogout: submitLogout
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Header);
+export default reduxForm({
+  form: headerSearchFormName
+})(connect(mapStateToProps, actions)(Header));
