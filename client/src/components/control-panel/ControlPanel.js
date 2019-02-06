546e236a3a070d7cf41749fc965e7abfcbaa7531
artyeug <artyeug@gmail.com>
2019-02-06 17:10:07 +0700

    Implement ControlPanel component

--- client/src/components/control-panel/ControlPanel.js
+++ client/src/components/control-panel/ControlPanel.js
@@ -1,12 +1,34 @@
 import './ControlPanel.sass';
 import React from 'react';
+import { BarGraph, Emerald, Home, Star, User } from '../svg';
+import Link from '../generic/Link';
 
 export default class ControlPanel extends React.Component {
   render() {
     return (
       <section className="control-panel">
-        <p>logo</p>
+        <div className="control-panel__logo-container">
+          <Emerald className="control-panel__logo" />
+        </div>
+        <nav className="control-panel__nav">
+          <ul className="ul">
+            <ListItem linkTo="/" iconComponent={Home} />
+            <ListItem linkTo="/users" iconComponent={User} />
+            <ListItem linkTo="/stock" iconComponent={Star} />
+            <ListItem linkTo="/charts" iconComponent={BarGraph} />
+          </ul>
+        </nav>
       </section>
     );
   }
 }
+
+const ListItem = ({
+  linkTo, iconComponent: SVGIcon
+}) => (
+  <li className="control-panel__li">
+    <Link className="control-panel__link" to={linkTo}>
+      <SVGIcon className="control-panel__svg" />
+    </Link>
+  </li>
+);
