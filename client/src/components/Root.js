a97c4cf1f63a8aca7fc1067ef5c4c4abc9c5dedd
artyeug <artyeug@gmail.com>
2019-02-07 06:30:35 +0700

    Implement NotificationSystem; add login trigger

--- client/src/components/Root.js
+++ client/src/components/Root.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { Switch, Route, BrowserRouter, withRouter } from 'react-router-dom';
+import { Provider, connect } from 'react-redux';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+  withRouter
+} from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { isLoggedIn } from '../reducers/user';
 import App from './App';
 import Login from './login/Login';
+import NotificationSystem from './notification-system';
 
 const Root = ({ store }) => (
   <Provider store={store}>
@@ -21,14 +29,27 @@ const Routes = withRouter(({ location }) => (
       classNames="fade"
     >
       <section className="route-section">
+        <NotificationSystem />
         <Switch location={location}>
           <Route path="/login" component={Login} />
           {/* <Route path="/resetPassword/:token" component={Login} /> */}
-          <Route path="/" component={App} />
+          <Route path="/" component={LoginTrigger} />
         </Switch>
       </section>
     </CSSTransition>
   </TransitionGroup>
 ));
+
+const mapStateToLoginTriggerProps = (state) => ({
+  isLoggedIn: isLoggedIn(state)
+});
+
+const LoginTrigger = connect(mapStateToLoginTriggerProps)(
+  ({ isLoggedIn }) => (
+    isLoggedIn
+      ? <App />
+      : <Redirect to="/login" />
+  )
+);
 
 export default Root;
