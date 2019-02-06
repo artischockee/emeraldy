8ce804da5ab656b951092adf73e6df84e8ca295c
artyeug <artyeug@gmail.com>
2019-02-06 17:37:34 +0700

    Delete unnecessary files

--- client/src/components/contact/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Contact = () => (
-  <React.Fragment>
-    <p className="just">Contact</p>
-  </React.Fragment>
-);
-
-export default Contact;
--- client/src/components/crud/Crud.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Crud = () => (
-  <React.Fragment>
-    <p className="just">CRUD</p>
-  </React.Fragment>
-);
-
-export default Crud;
