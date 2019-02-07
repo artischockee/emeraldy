5f6ee5da45de480005436a168b64fc5f822107f3
artyeug <artyeug@gmail.com>
2019-02-07 13:14:00 +0700

    Make the component stateless one

--- client/src/components/notification-system/NotificationSystem.js
+++ client/src/components/notification-system/NotificationSystem.js
@@ -1,30 +1,32 @@
 import './NotificationSystem.sass';
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getNotifications } from '../../reducers/notifications';
 import Notification from './Notification';
 
-class NotificationSystem extends React.Component {
-  render() {
-    const { notifications } = this.props;
+const NotificationSystem = ({ notifications }) => (
+  <div className="notifications-container">
+    {notifications && notifications.map((notification) => (
+      <Notification key={notification.id} {...notification} />
+    ))}
+  </div>
+);
 
-    return (
-      <div className="notifications-container">
-        {notifications && notifications.map((notification) => (
-          <Notification
-            key={notification.id}
-            {...notification}
-            // deleteNotification={() => deleteNotification(notification.id)}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+NotificationSystem.propTypes = {
+  notifications: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      mainText: PropTypes.arrayOf(PropTypes.string),
+      subtitle: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
 
 const mapStateToProps = (state) => ({
   notifications: getNotifications(state)
 });
-
 
 export default connect(mapStateToProps)(NotificationSystem);
